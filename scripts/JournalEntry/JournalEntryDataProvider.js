07bcbb927c809504c3929169a74ea56b67d36b3a
Mark McCann <mjmccann12@me.com>
2020-03-05 19:42:30 -0600

    one entry is rendering but not the second

--- scripts/JournalEntry/JournalEntryDataProvider.js
+++ scripts/JournalEntry/JournalEntryDataProvider.js
@@ -14,6 +14,13 @@ const journal = [
         concept: "HTML & CSS",
         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
         mood: "Ok"
+    },
+    {
+        id: 2,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
     }
 ]
 
