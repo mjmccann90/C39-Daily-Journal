5b50c7480154f3d2ba38c4f60bc7fcee22b1f7e5
Mark McCann <mjmccann12@me.com>
2020-03-06 15:46:26 -0600

    did some styling to h1s h3s h4s

--- scripts/JournalEntry/JournalEntry.js
+++ scripts/JournalEntry/JournalEntry.js
@@ -3,7 +3,7 @@ const EntryComponent = (journalEntryObject) => {
     <section id="entryObjContainer" entry--${journalEntryObject.id}>
         <div class ="entry-card">
                 <h3>Journal Date: ${journalEntryObject.date}</h3>
-                <p>Concepts Covered: ${journalEntryObject.concept}<>
+                <h5>Concepts Covered: ${journalEntryObject.concept}</h5>
                 <p>Journal Entry: ${journalEntryObject.entry}</p>
                 <p>Mood for the day: ${journalEntryObject.mood}</p>
         </div>
